74085c119e2ffb775f066e187371e42a0227c894
SPaCE69 <rahulnishad6969@gmail.com>
2026-09-23 12:21:50 +0530

    Auth Screen Complete

--- src/app/(root)/(tabs)/profile.tsx
+++ src/app/(root)/(tabs)/profile.tsx
@@ -1,10 +1,35 @@
-import { Text, View } from "react-native";
+import { useAuth, useUser } from "@clerk/expo";
+import { useRouter } from "expo-router";
+import { Alert, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ProfileScreen = () => {
+  const { user } = useUser();
+  const { signOut } = useAuth();
+  const router = useRouter();
+  const handleSignOut = () => {
+    Alert.alert("Sign out", "Are you sure you want to sign out?", [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Sign Out",
+        style: "destructive",
+        onPress: async () => {
+          await signOut();
+          router.replace("/(auth)/sign-in");
+        },
+      },
+    ]);
+  };
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-brand-body" edges={["top"]}>
+      <TouchableOpacity
+        className="bg-brand-blue py-4 px-8 self-start rounded-xl"
+        onPress={handleSignOut}
+      >
+        <Text className="text-white">Log Out</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -12,7 +12,7 @@ const Index = () => {
     return <Redirect href="/(root)/(tabs)" />;
   }
 
-  return <Redirect href="/(auth)/sign-up" />
+  return <Redirect href="/(auth)/sign-in" />
 
 };
 
